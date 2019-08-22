b44607734f503854fb7edde70cc646a99049d22f
Chanatip Deenang <chanatip@ip-10-150-1-197.ap-southeast-1.compute.internal>
2019-08-22 15:37:21 +0700

    add battlefield model

---
+++ src/models/battlefield.repository.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import RepositoryBuilder from 'sendit-mongoose-repository'
+
+const schemaDefinition = {
+  name: String,
+  status: {
+    type: String,
+    enum: ['AVAILABLE', 'ACTIVE'],
+  },
+  coordinate: {
+      X: Number,
+      Y: Number,
+  },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Fleet' },
+}
+
+export const builder = RepositoryBuilder('Battlefield', schemaDefinition)
+export default builder.Repository
--- src/models/user/user.repository.js
+++
@@ -1,8 +0,0 @@
-import UserModel from './user.model'
-import MongooseBaseRepository from '@spksoft/mongoose-repository'
-
-class UserRepository extends MongooseBaseRepository {
-
-}
-const instance = new UserRepository(UserModel)
-export default instance
